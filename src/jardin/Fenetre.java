ceacb69f3208d130d9cd7a5b4d39fbcecfe49a6b
Clément Alexandre <clmntlxndr@gmail.com>
2017-02-14 11:35:53 +0100

    Containers splittés

--- src/jardin/Fenetre.java
+++ src/jardin/Fenetre.java
@@ -6,11 +6,16 @@
 package jardin;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTable;
 
 /**
  *
@@ -26,31 +31,47 @@ public class Fenetre extends JFrame{
         //setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
-        label = new JLabel("COUCOU");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        JButton b = new JButton("+");
+        //label = new JLabel("COUCOU");
+        //label.setHorizontalAlignment(JLabel.CENTER);
+
+
+
+        Container containerPrincipal = this.getContentPane();
+        LayoutManager lm = new GridLayout(4,1);
+        containerPrincipal.setLayout(lm);
         
         
+        // 1ere ligne en rouge
+        Container premierContainerVert = new Container();
+        premierContainerVert.setLayout(new GridLayout(2,1));
+        premierContainerVert.add(new JLabel("Frigo"));
+        premierContainerVert.add(new JTable(5,2));
+        
+        containerPrincipal.add(premierContainerVert);
+        
+        // 2eme ligne en rouge
+        containerPrincipal.add(new JLabel("Plats à préparer"));
+        
+        // 3eme ligne en rouge
+        containerPrincipal.add(new JLabel("Plats préparés"));
+        
+        JButton b = new JButton("Voir le champ");
+
         // ATTENTION ! CLASSE ANONYME
         
-        b.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                afficher("Les copains");
-            }  
-        });
-        
-        
-        
-        
+//        b.addActionListener(new ActionListener(){
+//            public void actionPerformed(ActionEvent e) {
+//                afficher("Les copains");
+//            }  
+//        });
 
-        add(label, BorderLayout.CENTER);  
-        add(b, BorderLayout.SOUTH);
+        containerPrincipal.add(b, BorderLayout.SOUTH);
         
         setVisible(true);
     }
-    public void afficher(String str){
-        label.setText(str);
-    }
+//    public void afficher(String str){
+//        label.setText(str);
+//    }
     
     
 }
